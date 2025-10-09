4cc504af0b48d258aefeebdc8c3c820421b026e0
kipkoechnoah279-crypto <kipkoechnoah279@gmail.com>
2025-10-09 10:02:41 +0300

    Add files via upload

---
+++ for_loop_number_printing.c
@@ -0,0 +1,20 @@
+/*
+Name:Noah Kipkoech
+Reg no:PA106/G/28709/25
+Description:program using for loop to print numbers from 100 to 50 in descending order
+*/
+
+//preprocesser directive 
+#include <stdio.h>//scanf (),printf ()
+
+//main function
+int main()
+{
+//variable declaration 
+int i;
+for(int i=100;i>=50;i--)
+{
+printf ("%d\n",i);
+}
+return 0;
+}
