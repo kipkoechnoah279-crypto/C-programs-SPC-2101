a68b4c282a380d9a071cb31494e4a7c6a20ea474
kipkoechnoah279-crypto <kipkoechnoah279@gmail.com>
2025-10-23 13:14:07 +0300

    Add files via upload

---
+++ electric_bill_calculater.c
@@ -0,0 +1,49 @@
+/*
+Name:Noah Kipkoech 
+Reg no:PA106/G/25
+Description:program to calculate electric bill by taking units consumed from the user 
+*/
+//pre-processor directive 
+#include <stdio.h>//scanf (),printf ()
+
+int calculateElectricBill(int unitsConsumed) {
+    int totalBill = 0;
+
+    if (unitsConsumed <= 100) {
+        totalBill = unitsConsumed * 10;
+    } else {
+        totalBill += 100 * 10;
+        int remainingUnits = unitsConsumed - 100;
+
+        if (remainingUnits <= 100) {
+            totalBill += remainingUnits * 15;
+        } else {
+            totalBill += 100 * 15;
+            remainingUnits -= 100;
+
+            totalBill += remainingUnits * 20;
+        }
+    }
+
+    return totalBill;
+}
+//main function 
+int main() {
+    int units;
+    int billAmount;
+
+    printf("Enter the number of units consumed: ");
+
+    if (scanf("%d", &units) != 1 || units < 0) {
+        printf("Invalid input. Please enter a positive whole number.\n");
+        return 1;
+    }
+
+//function call
+    billAmount = calculateElectricBill(units);
+
+    printf("Units Consumed: %d\n", units);
+    printf("Total Bill Amount: KSh. %d\n", billAmount);
+    
+    return 0;
+}
